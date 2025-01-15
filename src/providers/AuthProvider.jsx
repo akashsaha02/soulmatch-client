84ca5935763f42e14e075a0053dd2f422204647e
Akash4056 <asaha8313@gmail.com>
2025-01-15 15:49:32 +0530

    feat: integrate JWT token handling in AuthProvider; use axios for token management on user authentication state change

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,7 +1,8 @@
 import { createContext, useEffect, useState } from 'react'
 import { createUserWithEmailAndPassword, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from 'firebase/auth';
 
-import auth  from '../firebase/firebase.init';
+import auth from '../firebase/firebase.init';
+import useAxiosPublic from './../hooks/useAxiosPublic';
 const googleProvider = new GoogleAuthProvider();
 
 
@@ -12,6 +13,7 @@ const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true);
     const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const axiosPublic = useAxiosPublic();
 
 
     const createUser = (email, password) => {
@@ -45,14 +47,32 @@ const AuthProvider = ({ children }) => {
 
 
     useEffect(() => {
-        const unsubscribe = onAuthStateChanged(auth, async (user) => {
-            setUser(user);
-            console.log(user);
+        const unsubscribe = onAuthStateChanged(auth, async (currentUser) => {
+            setUser(currentUser);
+            if (currentUser) {
+                // setIsAuthenticated(true);
+                const userInfo = {
+                    email: currentUser.email,
+                }
+                axiosPublic.post('/jwt', userInfo).then(res => {
+                    if(res.data.token){
+                        localStorage.setItem('access-token', res.data.token);
+                    }
+                })
+
+                //get token and store token
+            } else {
+                //do something
+                //remove token (if stored in client side)
+                localStorage.removeItem('access-token');
+            }
+
+            // console.log(user);
             setLoading(false);
         })
 
         return () => unsubscribe();
-    }, [])
+    }, [axiosPublic])
 
 
 
