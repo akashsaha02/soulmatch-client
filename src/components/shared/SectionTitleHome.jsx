b89ef6af3c17b5283b1a2866ac61363109fe6024
Akash4056 <asaha8313@gmail.com>
2025-01-17 00:12:52 +0530

    feat: enhance MyFavourites page with SectionTitleHome component; implement delete functionality for favourites

--- src/components/shared/SectionTitleHome.jsx
+++ src/components/shared/SectionTitleHome.jsx
@@ -1,10 +1,10 @@
 
 import flowerLogo from '../../assets/icons/flower-bouqet.png'
-const SectionTitleHome = () => {
+const SectionTitleHome = ({heading, subHeading}) => {
     return (
         <div className="pt-10 pb-5 flex flex-col items-center">
-            <p className="cinzel font-bold uppercase md:text-lg text-me-darkOrange">Trusted Brand</p>
-            <h2 className="text-3xl md:text-4xl font-bold playfair text-me-brown">Recent Couples</h2>
+            <p className="cinzel font-bold uppercase md:text-lg text-me-darkOrange">{subHeading}</p>
+            <h2 className="text-3xl md:text-4xl font-bold playfair text-me-brown">{heading}</h2>
             <div className="mt-1 md:mt-2">
                 <img src={flowerLogo} className='w-24 md:w-36'/>
             </div>
--- src/pages/dashboard/user/MyFavourites.jsx
+++ src/pages/dashboard/user/MyFavourites.jsx
@@ -1,12 +1,105 @@
+import SectionTitleHome from '@/components/shared/SectionTitleHome';
 import useFavourites from '@/hooks/useFavourites';
+import { FaTrashAlt } from 'react-icons/fa';
 import React from 'react'
+import useAxiosSecure from '@/hooks/useAxiosSecure';
+import Swal from 'sweetalert2';
 
 const MyFavourites = () => {
 
   const [favourites, refetch] = useFavourites();
+
+  const axiosSecure = useAxiosSecure();
+
+  const handleDeleteFavourite = (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!"
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axiosSecure.delete(`/favourites/${id}`).then((res) => {
+          if (res.data.deletedCount > 0) {
+            refetch();
+            Swal.fire({
+              title: "Deleted!",
+              text: "Your file has been deleted.",
+              icon: "success"
+            });
+          }
+        }).catch((err) => {
+          console.log(err);
+        });
+      }
+    });
+  }
   return (
-    <div>MyFavourites</div>
-    
+    <div>
+      <SectionTitleHome
+        heading='My Favourites'
+        subHeading='Liked profiles'
+      />
+
+      <div className="">
+
+          <div className="my-10">
+            <div className="overflow-x-auto max-w-3xl mx-auto">
+              <table className="table-auto w-full border-collapse bg-white shadow-md rounded-lg overflow-hidden">
+                {/* head */}
+                <thead>
+                  <tr className="text-lg font-semibold bg-indigo-600 text-white">
+                    <th className="px-6 py-3 text-left">Biodata Id</th>
+                    <th className="px-6 py-3 text-left">Image</th>
+                    <th className="px-6 py-3 text-left">Name</th>
+                    <th className="px-6 py-3 text-left">Action</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {/* row 1 */}
+                  {
+                    favourites.map((item, index) => {
+                      return (
+                        <tr key={item._id} className="hover:bg-gray-50">
+                          <td className="px-6 py-4 text-sm text-gray-700">{item.favouriteBiodataId}</td>
+                          <td className="px-6 py-4">
+                            <div className="avatar">
+                              <div className="mask mask-squircle h-12 w-12">
+                                <img
+                                  src={item.favouriteProfileImage}
+                                  alt={item.favouriteName}
+                                  className="object-cover w-full h-full rounded-full"
+                                />
+                              </div>
+                            </div>
+                          </td>
+                          <td className="px-6 py-4 text-sm text-gray-700">{item.favouriteName}</td>
+                          <td className="px-6 py-4">
+                            <button
+                              onClick={() => handleDeleteFavourite(item._id)}
+                              className="btn btn-ghost btn-md text-red-500 hover:text-red-700">
+                              <FaTrashAlt />
+                            </button>
+                          </td>
+                        </tr>
+                      );
+                    })
+                  }
+                </tbody>
+              </table>
+            </div>
+          </div>
+
+        </div>
+
+
+
+
+    </div>
+
   )
 }
 
