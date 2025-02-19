11bdc83b2cc29e0b25aee6c757054bf4b07fe3f1
Akash4056 <asaha8313@gmail.com>
2025-02-19 17:11:02 +0530

    feat: enhance Error page with navigation options and update routing to include error handling

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -27,7 +27,7 @@ export const router = createBrowserRouter([
     {
         path: "/",
         element: <Main />,
-        // errorElement: <Error />,
+        errorElement: <Error />,
         children: [
             {
                 path: "/",
